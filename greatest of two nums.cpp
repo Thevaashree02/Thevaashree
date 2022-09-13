df9f3e5fe15d2b2f463206191f2482056ceadf49
Thevaashree02 <113322607+Thevaashree02@users.noreply.github.com>
2022-09-13 10:45:38 +0530

    Add files via upload

---
+++ greatest of two nums.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class biggest{
+	int x,y;
+	public:
+		void input();
+		void display();
+};
+void biggest::input()
+{
+	cout<<"enter the first number x:";
+	cin>>x;
+	cout<<"enter the second  number y:";
+	cin>>y;
+}
+void biggest::display()
+{
+	if(x>y)
+	{
+		cout<<"x is the biggest number"<<endl;
+	}
+	else
+	{
+		cout<<"y is the biggest number";
+	}
+}
+int main()
+{
+	biggest a;
+	a.input();
+	a.display();
+	return 0;
+}
