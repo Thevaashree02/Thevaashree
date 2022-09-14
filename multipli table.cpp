db1479a8f64ee0ea99dc8a7cc9af0770003028b3
Thevaashree02 <113322607+Thevaashree02@users.noreply.github.com>
2022-09-14 11:38:05 +0530

    Add files via upload

---
+++ multipli table.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,range,n;
+	cout<<"enter an integer:";
+	cin>>n;
+	cout<<"enter a range:";
+	cin>>range;
+	for(i=1;i<=range;i++)
+	{
+		cout<<n<<"*"<<i<<"="<<n*1<<endl;
+	}
+	
+}
